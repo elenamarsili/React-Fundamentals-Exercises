ad2b7a8adc5f70043c990184091655567c1e4e20
Elena Marsili <e.marsili89@gmail.com>
2024-10-15 08:53:30 +0200

    controlled components 1

--- L3. State Management/4. Controlled Components I/Starter/src/App.js
+++ L3. State Management/4. Controlled Components I/Starter/src/App.js
@@ -1,7 +1,14 @@
+import {useState} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+  const [query, setQuery] = useState("")
+
+  const updateQuery = (event) => {
+    setQuery(event.target.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,8 +16,8 @@ const App = () => {
         <h1 className="App-title">ReactND - Coding Practice</h1>
       </header>
       <div className="container">
-        <input type="text" placeholder="Say Something" />
-        <p className="echo">Echo:</p>
+        <input type="text" placeholder="Say Something" value={query} onChange={updateQuery}/>
+        <p className="echo">Echo: {query}</p>
         <p>This should mirror the text you typed into the input field.</p>
       </div>
     </div>
